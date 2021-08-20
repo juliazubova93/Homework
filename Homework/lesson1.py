52ccde27fd4c3f879e103d583f1c6cec80f7b82c
juliazubova93 <juliazubova93@mail.ru>
2021-08-20 17:30:17 +0300

    Подготовила первое задание

---
+++ Homework/lesson1.py
@@ -0,0 +1,14 @@
+# задание 1
+
+name = "Anna"
+age = 28
+number = 3.4
+print(name)
+print(age)
+print(number)
+
+name = input("What is your name?")
+print("Nice to meet you," + name)
+age = input ("How old are you?")
+print("Great! You are" + age + "years old")
+
